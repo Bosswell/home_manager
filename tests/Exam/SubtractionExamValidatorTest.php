2c578657ee62e31e4f7efd9b86dd09ee871a9f36
Bosswell <bosswell9@gmail.com>
2020-10-07 20:35:08 +0200

    added test

--- tests/Exam/SubtractionExamValidatorTest.php
+++ tests/Exam/SubtractionExamValidatorTest.php
@@ -16,11 +16,6 @@ class SubtractionExamValidatorTest extends TestCase
     {
         $examValidator = new SubtractionExamValidator();
         $examValidator
-            ->setUserQuestionsSnippets([
-                new UserQuestionSnippet(['questionId' => 1, 'checkedOptions' => [2, 5]]),
-                new UserQuestionSnippet(['questionId' => 2, 'checkedOptions' => [2, 3]]),
-                new UserQuestionSnippet(['questionId' => 3, 'checkedOptions' => [1]]),
-            ])
             ->setCorrectOptions([
                 1 => [1, 2],
                 2 => [1, 4, 2, 3],
@@ -33,6 +28,11 @@ class SubtractionExamValidatorTest extends TestCase
     public function testGetExamResult(): void
     {
         $result = self::$examValidator
+            ->setUserQuestionsSnippets([
+                new UserQuestionSnippet(['questionId' => 1, 'checkedOptions' => [2, 5]]),
+                new UserQuestionSnippet(['questionId' => 2, 'checkedOptions' => [2, 3]]),
+                new UserQuestionSnippet(['questionId' => 3, 'checkedOptions' => [1]]),
+            ])
             ->validate()
             ->toArray();
 
@@ -40,5 +40,22 @@ class SubtractionExamValidatorTest extends TestCase
         $this->assertEquals(4, $result['correctPoints']);
         $this->assertEquals(1, $result['incorrectPoints']);
         $this->assertEquals(42.86, $result['percentage']);
+    }
+
+    public function testGetExamResultWithMoreIncorrectThenCorrect(): void
+    {
+        $result = self::$examValidator
+            ->setUserQuestionsSnippets([
+                new UserQuestionSnippet(['questionId' => 1, 'checkedOptions' => [5, 4, 5, 6, 7]]),
+                new UserQuestionSnippet(['questionId' => 2, 'checkedOptions' => [2, 3, 5, 8]]),
+                new UserQuestionSnippet(['questionId' => 3, 'checkedOptions' => [2]]),
+            ])
+            ->validate()
+            ->toArray();
+
+        $this->assertEquals(7, $result['totalPoints']);
+        $this->assertEquals(2, $result['correctPoints']);
+        $this->assertEquals(8, $result['incorrectPoints']);
+        $this->assertEquals(0, $result['percentage']);
     }
 }
